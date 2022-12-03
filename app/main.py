c3a08eab539fc5a326c404a638620acb83852755
Ubuntu <ubuntu@ip-172-31-36-116.ap-south-1.compute.internal>
2022-12-03 10:54:16 +0530

    fix: mrz endpoint

--- app/main.py
+++ app/main.py
@@ -8,6 +8,14 @@ from datetime import datetime
 from dotenv import load_dotenv
 from deepface import DeepFace
 
+from passporteye import read_mrz
+import datetime
+import time
+import io
+import base64
+import sys
+from base64 import decodebytes
+
 # Use load_env to trace the path of .env:
 load_dotenv('.env')
 
@@ -43,3 +51,20 @@ def face():
     detector_backend = backends[4])
 
   return {"success": (result["verified"])}
+
+
+@app.route("/mrz", methods = ['POST'])
+def mrz():
+  body = request.json
+  b64_encoded_image = body["img"].split(",")
+  mrz = read_mrz(io.BytesIO(base64.decodebytes(bytes(b64_encoded_image[len(b64_encoded_image)-1], "utf-8")))).__dict__
+  passport_dob = mrz["date_of_birth"]
+  short_year = int(passport_dob[0:2])
+  month = int(passport_dob[2:4])
+  day = int(passport_dob[5:6])
+  year = 2000 + short_year
+  if short_year > 30:
+    year = 1900 + short_year
+  date_time = datetime.datetime(year, month, day)
+  age_in_epochs = int((time.mktime(date_time.timetuple())))
+  return {"birthyear": year}
